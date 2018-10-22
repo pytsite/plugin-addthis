40feaafa5a8f29753e7488ffb8eee687e5be722d
Oleksandr Shepetko <a@shepetko.com>
2018-10-22 20:54:37 +0300

    Release 2.2

--- res/assets/index.js
+++ res/assets/index.js
@@ -1,5 +1,6 @@
-const assetman = require('@pytsite/assetman');
+import assetman from '@pytsite/assetman';
+import setupWidget from '@pytsite/widget';
 
-require('@pytsite/widget').onWidgetLoad('plugins.addthis._widget.AddThis', (widget) => {
+setupWidget('plugins.addthis._widget.AddThis', widget => {
     assetman.loadJS('//s7.addthis.com/js/300/addthis_widget.js#pubid=' + widget.data('pubId'))
 });
